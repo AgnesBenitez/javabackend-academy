3948616b7e6f0e04c81bcbe2c2e32139b5088be5
AgnesBenitez <83478016+AgnesBenitez@users.noreply.github.com>
2021-08-17 16:24:32 -0500

    subida de ejercicio colections

---
+++ Colecciones/MapEQ.java
@@ -0,0 +1,22 @@
+package holamundo;
+
+import java.util.*;
+class MapEQ {
+ public static void main(String[] args) {
+ Map<ToDos, String> m = new HashMap<ToDos, String>();
+ ToDos t1 = new ToDos("Monday");
+ ToDos t2 = new ToDos("Monday");
+ ToDos t3 = new ToDos("Tuesday");
+ m.put(t1, "doLaundry");
+ m.put(t2, "payBills");
+ m.put(t3, "cleanAttic");
+ System.out.println(m.size());
+} }
+class ToDos{
+ String day;
+ ToDos(String d) { day = d; }
+ public boolean equals(Object o) {
+ return ((ToDos)o).day == this.day;
+ }
+ 
+}
