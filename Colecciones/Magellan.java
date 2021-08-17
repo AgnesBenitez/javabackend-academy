4b4b31d37cad877e8924ca232ac318068e548047
AgnesBenitez <83478016+AgnesBenitez@users.noreply.github.com>
2021-08-17 17:46:51 -0500

    se sube ejercicio

---
+++ Colecciones/Magellan.java
@@ -0,0 +1,20 @@
+package holamundo;
+
+import java.util.*;
+ public class e2{
+ public static void main(String[] args) {
+ TreeMap<String, String> myMap = new TreeMap<String, String>();
+ myMap.put("a", "apple"); myMap.put("d", "date");
+ myMap.put("f", "fig"); myMap.put("p", "pear");
+ System.out.println("1st after mango: " + // sop 1
+ myMap.higherKey("f"));
+ System.out.println("1st after mango: " + // sop 2
+ myMap.ceilingKey("f"));
+ System.out.println("1st after mango: " + // sop 3
+ myMap.floorKey("f"));
+ SortedMap<String, String> sub = new TreeMap<String, String>();
+ sub = myMap.tailMap("f");
+ System.out.println("1st after mango: " + // sop 4
+ sub.firstKey());
+ }
+}
